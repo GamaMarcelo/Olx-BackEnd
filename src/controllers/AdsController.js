e99b0065d4177d188874bf277c51fcb97184a243
mistergama <marcelo.allan@gmail.com>
2023-02-24 17:26:07 -0300

    Criando a estrutura do controllers

---
+++ src/controllers/AdsController.js
@@ -0,0 +1,17 @@
+module.exports = {
+    getCategories: async (req, res) => {
+
+    },
+    addAction: async (req, res) => {
+
+    },
+    getList: async (req, res) => {
+
+    },
+    getItem: async(req, res) => {
+
+    },
+    aditAction: async (req, res) => {
+
+    },
+}
